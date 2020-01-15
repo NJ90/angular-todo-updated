14782087f41dcfe039f6b9d6025f0e1e80603af5
NJ90 <jangnamee@gmail.com>
2020-01-15 16:29:59 +0900

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBxAzyqMINYiixbsjOsKfkW3UnIwOJN3hM",
+    authDomain: "todo-project-nj.firebaseapp.com",
+    databaseURL: "https://todo-project-nj.firebaseio.com",
+    projectId: "todo-project-nj",
+    storageBucket: "todo-project-nj.appspot.com",
+    messagingSenderId: "984132264849",
+    appId: "1:984132264849:web:691313fa8a4c84fb023030",
+    measurementId: "G-YQ4WEVT3HQ"
+  }
 };
