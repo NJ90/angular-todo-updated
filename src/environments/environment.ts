8b70b0bcd5633c3fb2055e81ad965a13060975f5
NJ90 <jangnamee@gmail.com>
2020-01-15 16:29:48 +0900

    firebase 배포

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBxAzyqMINYiixbsjOsKfkW3UnIwOJN3hM",
+    authDomain: "todo-project-nj.firebaseapp.com",
+    databaseURL: "https://todo-project-nj.firebaseio.com",
+    projectId: "todo-project-nj",
+    storageBucket: "todo-project-nj.appspot.com",
+    messagingSenderId: "984132264849",
+    appId: "1:984132264849:web:691313fa8a4c84fb023030",
+    measurementId: "G-YQ4WEVT3HQ"
+  }
 };
 
 /*
